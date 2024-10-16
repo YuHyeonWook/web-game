1ee2dcafa183b01f2d88a2438a69be5605a5bb33
wook <yho7955@naver.com>
2024-10-16 13:32:24 +0900

    feat: germs 게임 구현

---
+++ src/components/GameComponent.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import Phaser from "phaser";
+import Boot from "./Boot";
+import Preloader from "./Preloader";
+import MainMenu from "./MainMen";
+import MainGame from "./Game";
+
+const GameComponent = () => {
+  useEffect(() => {
+    const config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      backgroundColor: "#000000",
+      parent: "phaser-game",
+      scene: [Boot, Preloader, MainMenu, MainGame],
+      physics: {
+        default: "arcade",
+        arcade: { debug: false },
+      },
+    };
+    const game = new Phaser.Game(config);
+    return () => {
+      game.destroy(true);
+    };
+  }, []);
+
+  return <div id="phaser-game"></div>;
+};
+
+export default GameComponent;
